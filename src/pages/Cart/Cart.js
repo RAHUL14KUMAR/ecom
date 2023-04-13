0178b0f0011ecb25802b5101f6843918c73fefd9
rahul14kumar <rahul.kumar.it25@heritageit.edu.in>
2023-04-14 02:05:26 +0530

    folder structure

---
+++ src/pages/Cart/Cart.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function cart() {
+  return (
+    <div>
+      cart
+      
+    </div>
+  )
+}
+
+export default cart
---
+++ src/pages/admin/Admin.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import style from "./Admin.module.scss"
+function Admin() {
+  return (
+    <div>
+      Admin
+    </div>
+  )
+}
+
+export default Admin
